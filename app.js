98eaa86e2c9dac5dd3dc295cd369c8ca516d3f48
ahmed.ismail.lalwani@gmail.com <ahmed.ismail.lalwani@gmail.com>
2023-09-01 21:15:03 +0500

    hello world

---
+++ app.js
@@ -0,0 +1,70 @@
+//   CHAPTER NO 01       
+//  Q NO 01
+
+/*var Message = "Error! Please enter a valid password.";
+alert(Message);*/
+
+// Q NO 02
+
+/*var Message = "Welcome to js land..\n Hapay coding!";
+alert(Message);*/
+
+//Q No 03
+
+/*var Message = "Welcome to js land....";
+alert(Message);*/
+
+//Q No 04
+
+// var Message = "prevent this page from creating additioal dialogs";
+//   alert(Message);
+
+//Q No 05
+
+/*var Message = "Hello..i can run js through my web browser's console ";
+ alert(Message);
+                               
+// CHAPTER NO 02 
+                               
+//  Q No 01
+
+//var Message ="Dhone Doe";
+//alert(Message);
+
+//Q No 02
+
+// var Message ="15 Years old";
+//alert(Message);
+                                   
+
+//Q No 03
+  //var Message ="Certified Mobile Application Development";
+  //alert(Message);
+                              
+
+//Q No 4
+
+//var Message ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(Message);
+
+//Q No 05
+
+//var Message ="email adress ak5589667@gmail.com";
+// alert(Message);
+
+//Q No 06
+
+//var Message ="i am trying to learn form the BOOk A Smarter\nWay to learn javascript";
+//alert(Message);
+
+//Q No 07
+
+//var Message ="Yah! i can write HTML content through javasscript";
+//alert(Message);
+
+ //Q No 08
+
+// var Message ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(Message);
+
+// end
